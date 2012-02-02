7fbe5f677b1f799853156743e3c11fe627f38240
mgauto504 <mgauto504@LB202-03-W7.oconnell.org>
2012-02-02 14:24:39 -0500

    Documented Parts of HouseSocket System

--- src/main/java/Network/HouseServerCommunicationThread.java
+++ src/main/java/Network/HouseServerCommunicationThread.java
@@ -20,13 +20,39 @@ import java.util.logging.Logger;
  * @author Manuel Gauto
  */
 public class HouseServerCommunicationThread implements Runnable {
-
+    /**
+     * Socket to use to communicate with client
+     */
     private Socket socket;
+    
+    /**
+     * Used to accept input from socket
+     */
     private InputStream InputStream;
+    
+    /**
+     * Used to send information to client through socket
+     */
     private OutputStream OutputStream;
+    
+    /**
+     * Send objects through socket
+     */
     private ObjectOutputStream ObjectOutputStream;
+    
+    /**
+     * Accept objects from clients
+     */
     private ObjectInputStream ObjectInputStream;
+    
+    /**
+     * Read strings from InputStream
+     */
     private Scanner Scanner;
+    
+    /**
+     * Possible RequestTypes for socket
+     */
     private RequestTypes RequestTypes;
 
     public HouseServerCommunicationThread(Socket socket) {
--- src/main/java/Network/HouseSocket.java
+++ src/main/java/Network/HouseSocket.java
@@ -11,28 +11,55 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Socket to communicate with extenders
  * @author Manuel Gauto
  */
 public class HouseSocket implements Runnable{
+    /**
+     * Socket to accept connections
+     */
     private ServerSocket ServerSocket;
-    private final int PORT=6356;
+    
+    /**
+     * Port to listen on
+     */
+    private int port=6356;
+    
+    /**
+     * Socket that will be used to 
+     * communicate with the client
+     */
     private Socket socket;
 
+    /**
+     * Initialize HouseSocket with specified port
+     * @param port Port to use
+     */
+    public HouseSocket(int port){
+        //Set port number
+        this.port=port;
+    }
     @Override
     public void run() {
         try {
-            ServerSocket=new ServerSocket(PORT);
+            //Initialize Socket
+            ServerSocket=new ServerSocket(port);
         } catch (IOException ex) {
+            //Port is probably in use or some other error(IOException)
             Logger.getLogger(HouseSocket.class.getName()).log(Level.SEVERE, null, ex);
         }
+        //Loop forever :)
         while(true){
             try {
+                //Client Connected
                 socket=ServerSocket.accept();
             } catch (IOException ex) {
+                //Some weird error (IOException)
                 Logger.getLogger(HouseSocket.class.getName()).log(Level.SEVERE, null, ex);
             }
+            //Create new communication thread
             Thread t=new Thread(new HouseServerCommunicationThread(socket));
+            //Start the thread
             t.start();
         }
     }
